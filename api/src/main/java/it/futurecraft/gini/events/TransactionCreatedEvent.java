4302112051b96bbb322965990b299feb199b8a68
gugliettidaniele <gugliettidaniele@gmail.com>
2024-06-11 00:16:43 +0200

    feat: now `TransactionCreatedEvent` and `TransactionDeletedEvent` extends bukkit's `Event` class ✨

--- api/src/main/java/it/futurecraft/gini/events/TransactionCreatedEvent.java
+++ api/src/main/java/it/futurecraft/gini/events/TransactionCreatedEvent.java
@@ -1,17 +1,39 @@
 package it.futurecraft.gini.events;
 
 import it.futurecraft.gini.transactions.Transaction;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * The event is notified when a transaction is created.
  *
  * @see Transaction
  */
-public interface TransactionCreatedEvent {
+public final class TransactionCreatedEvent extends Event {
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    public static @NotNull HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+    private final Transaction transaction;
+
+    public TransactionCreatedEvent(Transaction transaction) {
+        this.transaction = transaction;
+    }
+
     /**
      * The transaction created in this event.
      *
      * @return The transaction.
      */
-    Transaction transaction();
+    public @NotNull Transaction transaction() {
+        return this.transaction;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLERS;
+    }
 }
--- api/src/main/java/it/futurecraft/gini/events/TransactionDeletedEvent.java
+++ api/src/main/java/it/futurecraft/gini/events/TransactionDeletedEvent.java
@@ -1,12 +1,39 @@
 package it.futurecraft.gini.events;
 
 import it.futurecraft.gini.transactions.Transaction;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * The event is notified when a transaction is deleted.
  *
  * @see Transaction
  */
-public interface TransactionDeletedEvent {
-    Transaction transaction();
+public final class TransactionDeletedEvent extends Event {
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    public static @NotNull HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+    private final Transaction transaction;
+
+    public TransactionDeletedEvent(Transaction transaction) {
+        this.transaction = transaction;
+    }
+
+    /**
+     * The transaction deleted in this event.
+     *
+     * @return The transaction.
+     */
+    public @NotNull Transaction transaction() {
+        return this.transaction;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLERS;
+    }
 }
